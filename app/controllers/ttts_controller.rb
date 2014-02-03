cd4d4b28b06661cb4982d1b97d71e0d94e937b50
mantiscy <mantiscy@gmail.com>
2014-02-03 15:38:48 +0000

    Dried up controller, moved code to Ttt model

--- app/controllers/ttts_controller.rb
+++ app/controllers/ttts_controller.rb
@@ -19,7 +19,7 @@ class TttsController < ApplicationController
   def create
 
     @ttt = Ttt.new(params[:ttt])
-    if set_initial_settings(@ttt)
+    if @ttt.set_initial_settings(current_user)
       if @ttt.save
         @ttt.users << current_user
         flash[:notice] = "Created new game"
@@ -35,31 +35,36 @@ class TttsController < ApplicationController
     @ttt = Ttt.find(params[:id])
     if @ttt.need_player == 'y'
       if @ttt.p1.to_i != current_user.id
-        @ttt = set_player_2(@ttt)
+        @ttt.set_player_2(current_user)
         @ttt.save     
       end 
     end 
+    @game_states = @ttt.populate_game
 
-    @game_states = populate_game(@ttt)
   end
 
   def play_comp
     @ttt = Ttt.find(params[:id])
     if @ttt.need_player == 'y'
-      @ttt = set_comp_player(@ttt)
+      @ttt.set_comp_player
       @ttt.save
     end
-    @game_states = populate_game(@ttt)
+    @game_states = @ttt.populate_game
     redirect_to @ttt
   end
 
   def update
-    if @ttt.need_player == 'y'
-      flash[:notice] = "Need another player"
+    if @ttt.check_completed
+      flash[:notice] = "Game has finished"
       redirect_to @ttt
     else
-      state = validate_move(@ttt)
-      check_game_status(@ttt, state)
+      if @ttt.need_player == 'y'
+        flash[:notice] = "Need another player"
+        redirect_to @ttt
+      else
+        state = validate_move(@ttt)
+        check_game_status(@ttt, state)
+      end
     end
   end
 
@@ -71,24 +76,23 @@ class TttsController < ApplicationController
 
   #----------------------------------------------------------------#
 
-  def check_completed(game)
-    game.completed == 'y'
-  end
-
   def check_game_status(game, state)
 
       if state
         if state.save
           game.states << state
-          game = set_turn(game)
+          game.set_turn
           if game.save
-            @game_states = populate_game(game)
-            if win = winner(game, @game_states)
+            @game_states = game.populate_game
+            if win = game.winner(@game_states)
               game.completed = 'y'
+              game.save
               if win == 'Draw'
                 flash[:notice] = "Game is a draw!"
+              elsif game.p2 == "-1" && !game.states.last.p1
+                flash[:notice] = "PC has won the game!"
               else
-              flash[:notice] = "#{win.email} has won the game!"
+                flash[:notice] = "#{current_user.email} has won the game!"  
             end
               redirect_to game
             else
@@ -106,141 +110,17 @@ class TttsController < ApplicationController
     
   end
 
-  def set_initial_settings(game)
-
-    # if game.need_player == 'y'
-    #   game.p2 = current_user.id.to_s
-    #   game.need_player = 'n'
-    #   return true
-    if game.need_player == nil
-      game.p1 = current_user.id.to_s
-      game.completed = 'n'
-      game.need_player = 'y'
-      game.turn_for_player_id = game.p1
-      return true
-    else
-      return false
-    end
-    
-  end
-
-  def set_player_2(game)
-    game.p2 = current_user.id.to_s
-    game.need_player = 'n'
-    game.users << current_user
-    return game
-  end
-
-  def set_comp_player(game)
-    game.p2 = "-1"
-    game.need_player = 'n'
-    game.users << User.create(email: 'pc')
-    return game
-  end
-
-  def populate_game(game)
-
-    game_states = {a1: "", a2: "", a3: "", b1: "", b2: "", b3: "", c1: "", c2: "", c3: ""}
-
-    game.states.each do |state|
-      if state.p1
-        game_states[state.pos.to_sym] = 'x'
-      else
-        game_states[state.pos.to_sym] = 'o'
-      end
-    end
-    return game_states
-  end
-
   def validate_move(game)
-
-    # if params[:pc_move] == 'y' && game.p2 = '-1'
-    #   state = State.new
-    #   pos = get_pc_move(game)
-    #   state.pos = pos
-    #   state.p1 = false
-    # end
-    if check_completed(game)
+    if game.check_completed
       flash[:notice] = "Game has ended. No more moves"
       redirect_to game
     elsif game.turn_for_player_id.to_i == current_user.id
-      state = State.new
-      if current_user.id == game.p1.to_i
-        state.pos = params[:pos]
-        state.p1 = true
-        return state
-      elsif game.p2 == '-1'
-        pos = get_pc_move(game)
-        state.pos = pos
-        state.p1 = false
-        return state
-      else
-        state.pos = params[:pos]
-        state.p1 = false
-        return state
-      end
+      return game.state(params[:pc_move], params[:pos], current_user)
     elsif params[:pc_move] == 'y' && game.p2 = '-1'
-      state = State.new
-      pos = get_pc_move(game)
-      state.pos = pos
-      state.p1 = false
-      return state
+      return game.state(params[:pc_move], params[:pos], current_user)
     else
       return false
     end
-  end
-
-  def get_pc_move(game)
-    game_states = populate_game(game)
-    game_states.each do |pos, val|
-      if
-        val == ""
-        return pos
-      end
-    end
-  end
-
-  def set_turn(game)
-    if game.states.last.p1
-      game.turn_for_player_id = game.p2
-    else
-      game.turn_for_player_id = game.p1
-    end
-    return game
-  end
-
-  def winner(game, game_states)
-
-    if game_states.fetch(:a1) == game_states.fetch(:a2) && game_states.fetch(:a1) == game_states.fetch(:a3) && game_states.fetch(:a1) != ""
-      # binding.pry
-      return current_user
-    elsif game_states.fetch(:b1) == game_states.fetch(:b2) && game_states.fetch(:b1) == game_states.fetch(:b3) && game_states.fetch(:b1) != ""
-      # binding.pry
-      return current_user
-    elsif game_states.fetch(:c1) == game_states.fetch(:c2) && game_states.fetch(:c1) == game_states.fetch(:c3) && game_states.fetch(:c1) != ""
-      # binding.pry
-      return current_user
-    elsif game_states.fetch(:a1) == game_states.fetch(:b1) && game_states.fetch(:a1) == game_states.fetch(:c1) && game_states.fetch(:a1) != ""
-      # binding.pry
-      return current_user
-    elsif game_states.fetch(:a2) == game_states.fetch(:b2) && game_states.fetch(:a2) == game_states.fetch(:c2) && game_states.fetch(:a2) != ""
-      # binding.pry
-      return current_user
-    elsif game_states.fetch(:a3) == game_states.fetch(:b3) && game_states.fetch(:a3) == game_states.fetch(:c3) && game_states.fetch(:a3) != ""
-      # binding.pry
-      return current_user
-    elsif game_states.fetch(:a1) == game_states.fetch(:b2) && game_states.fetch(:a1) == game_states.fetch(:c3) && game_states.fetch(:a1) != ""
-      # binding.pry
-      return current_user
-    elsif game_states.fetch(:a3) == game_states.fetch(:b2) && game_states.fetch(:a3) == game_states.fetch(:c1) && game_states.fetch(:a2) != ""
-      # binding.pry
-      return current_user
-    elsif game.states.count == 9
-      return "Draw"
-    else
-      return false
-    end
-      
   end
 
 
--- app/models/ttt.rb
+++ app/models/ttt.rb
@@ -4,5 +4,127 @@ class Ttt < ActiveRecord::Base
   attr_accessor :last_move
 
   has_and_belongs_to_many :users
-  has_many :states
+  has_many :states, order: :created_at
+
+
+  def set_initial_settings(current_user)
+    if need_player == nil
+      self.p1 = current_user.id.to_s
+      self.completed = 'n'
+      self.need_player = 'y'
+      self.turn_for_player_id = self.p1
+      return true
+    else
+      return false
+    end
+  end
+
+  def state(pc_move, pos, current_user)
+    state = State.new
+    if self.turn_for_player_id.to_i == current_user.id
+      if current_user.id == self.p1.to_i
+        state.pos = pos
+        state.p1 = true
+      elsif self.p2 == '-1'
+        pos = self.get_pc_move
+        state.pos = pos
+        state.p1 = false
+      else
+        state.pos = pos
+        state.p1 = false
+      end
+    elsif pc_move == 'y' && self.p2 = '-1'
+      pos = self.get_pc_move
+      state.pos = pos
+      state.p1 = false
+    end
+    
+    state
+  end
+
+  def set_player_2(current_user)
+    self.p2 = current_user.id.to_s
+    self.need_player = 'n'
+    self.users << current_user
+    return   
+  end
+
+  def set_comp_player
+    self.p2 = "-1"
+    self.need_player = 'n'
+    self.users << User.create(email: 'pc')
+    return
+  end
+
+  def populate_game
+
+    game_states = {a1: "", a2: "", a3: "", b1: "", b2: "", b3: "", c1: "", c2: "", c3: ""}
+
+    self.states.each do |state|
+      if state.p1
+        game_states[state.pos.to_sym] = 'x'
+      else
+        game_states[state.pos.to_sym] = 'o'
+      end
+    end
+    return game_states
+  end
+
+  def get_pc_move
+    game_states = self.populate_game
+    game_states.each do |pos, val|
+      if
+        val == ""
+        return pos
+      end
+    end
+  end
+
+  def set_turn
+    if self.states.last.p1
+      self.turn_for_player_id = self.p2
+    else
+      self.turn_for_player_id = self.p1
+    end
+    return 
+  end
+
+  def check_completed
+    self.completed == 'y'
+  end
+
+  def winner(game_states)
+
+    if game_states.fetch(:a1) == game_states.fetch(:a2) && game_states.fetch(:a1) == game_states.fetch(:a3) && game_states.fetch(:a1) != ""
+      
+      return true
+    elsif game_states.fetch(:b1) == game_states.fetch(:b2) && game_states.fetch(:b1) == game_states.fetch(:b3) && game_states.fetch(:b1) != ""
+      
+      return true
+    elsif game_states.fetch(:c1) == game_states.fetch(:c2) && game_states.fetch(:c1) == game_states.fetch(:c3) && game_states.fetch(:c1) != ""
+      
+      return true
+    elsif game_states.fetch(:a1) == game_states.fetch(:b1) && game_states.fetch(:a1) == game_states.fetch(:c1) && game_states.fetch(:a1) != ""
+      
+      return true
+    elsif game_states.fetch(:a2) == game_states.fetch(:b2) && game_states.fetch(:a2) == game_states.fetch(:c2) && game_states.fetch(:a2) != ""
+      
+      return true
+    elsif game_states.fetch(:a3) == game_states.fetch(:b3) && game_states.fetch(:a3) == game_states.fetch(:c3) && game_states.fetch(:a3) != ""
+      
+      return true
+    elsif game_states.fetch(:a1) == game_states.fetch(:b2) && game_states.fetch(:a1) == game_states.fetch(:c3) && game_states.fetch(:a1) != ""
+      
+      return true
+    elsif game_states.fetch(:a3) == game_states.fetch(:b2) && game_states.fetch(:a3) == game_states.fetch(:c1) && game_states.fetch(:a3) != ""
+      
+      return true
+    elsif self.states.count == 9
+      return "Draw"
+    else
+      return false
+    end
+      
+  end
+
 end
